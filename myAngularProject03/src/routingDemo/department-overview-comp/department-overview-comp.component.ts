15806d76984d800a6ee4a536f223fad568431ba7
ShubhankariPadhi <https://github.com/ShubhankariPadhi/myshoppingcart.git>
2020-02-11 19:13:19 +0530

    "child routing "

---
+++ myAngularProject03/src/routingDemo/department-overview-comp/department-overview-comp.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-department-overview-comp',
+  template: `
+    <p>
+      department-overview-comp works!
+    </p>
+  `,
+  styles: []
+})
+export class DepartmentOverviewCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
