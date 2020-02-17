b83d6578b2ab893542e36786e72d0ca5d6843a49
ShubhankariPadhi <https://github.com/ShubhankariPadhi/myshoppingcart.git>
2020-02-17 19:14:33 +0530

    "including custom selector of parentStudentComp in root comp"

--- myAngularProject03/src/app/app.module.ts
+++ myAngularProject03/src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import {StudentListComponent} from "../studentRouting/student-list/student-list.component";
 import {ParentCompComponent} from "../parent-comp/parent-comp.component";
 import {ChildcompComponent} from "../parent-comp/childcomp/childcomp.component";
+import {StudentListCompComponent} from "../taskDir/student-list-comp/student-list-comp.component";
+import {ParentStudentComponent} from "../taskUsingParentChildDS/parent-student/parent-student.component";
+import {StudentMarkChildComponent} from "../taskUsingParentChildDS/student-mark-child/student-mark-child.component";
+import {StudentEditChildComponent} from "../taskUsingParentChildDS/student-edit-child/student-edit-child.component";
 /*import {StudentServiceService} from "../studentRouting/student-service.service";*/
 /*import {DepartmentListComponent} from '../routingDemo/department-list/department-list.component';
 import {EmployeeListComponent} from '../routingDemo/employee-list/employee-list.component';*/
@@ -18,7 +22,11 @@ import {EmployeeListComponent} from '../routingDemo/employee-list/employee-list.
     routingComponents,
     StudentListComponent,
     ParentCompComponent,
-    ChildcompComponent
+    ChildcompComponent,
+    StudentListCompComponent,
+    ParentStudentComponent,
+    StudentMarkChildComponent,
+    StudentEditChildComponent
 
       ],
   imports: [
