00c69361dbf9e0ffe622216ade6cb5b74cd7a678
ShubhankariPadhi <https://github.com/ShubhankariPadhi/myshoppingcart.git>
2020-02-17 19:16:03 +0530

    "student data list and mark list"

---
+++ myAngularProject03/src/taskUsingParentChildDS/student-data-for-comp.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentDataForCompService {
+  getStudentData(){
+    return [
+      {"id":1, "name":"subha","address":{"area": 'srnagar',"zipcode":500016}},
+      {"id":2,"name":"latus","address":{"area": 'srnagar',"zipcode":500016}},
+      {"id":3, "name":"akhil","address":{"area": 'balkampet',"zipcode":500016}},
+      {"id":4,"name":"gufs","address":{"area": 'yusufguda',"zipcode":500019}},
+      {"id":5, "name":"bhranti","address":{"area": 'koti',"zipcode":500002}},
+      {"id":6,"name":"anil","address":{"area": 'srnagar',"zipcode":500016}},
+    ];
+  }
+  getStudentMarks(){
+    return [
+      {"id":1,"subjects":{"english": 79,"math":88,"physics":95,"chemistry":92,"IT":97}},
+      {"id":2,"subjects":{"english": 85,"math":77,"physics":87,"chemistry":87,"IT":95}},
+      {"id":3,"subjects":{"english": 83,"math":86,"physics":83,"chemistry":93,"IT":96}},
+      {"id":4,"subjects":{"english": 77,"math":84,"physics":87,"chemistry":92,"IT":94}},
+      {"id":5,"subjects":{"english": 76,"math":83,"physics":86,"chemistry":93,"IT":93}},
+      {"id":6,"subjects":{"english": 86,"math":77,"physics":87,"chemistry":93,"IT":95}},
+    ];
+  }
+  /*getStudentData(){
+    return [
+      {"id":1, "name":"subha","area": 'srnagar',"zipcode":500016},
+      {"id":2,"name":"latus","area": 'srnagar',"zipcode":500016},
+      {"id":3, "name":"akhil","area": 'balkampet',"zipcode":500016},
+      {"id":4,"name":"gufs","area": 'yusufguda',"zipcode":500019},
+      {"id":5, "name":"bhranti","area": 'koti',"zipcode":500002},
+      {"id":6,"name":"anil","area": 'srnagar',"zipcode":500016},
+    ];
+  }
+  getStudentMarks(){
+    return [
+      {"id":1,"english": 79,"math":88,"physics":95,"chemistry":92,"IT":97},
+      {"id":2,"english": 85,"math":77,"physics":87,"chemistry":87,"IT":95},
+      {"id":3,"english": 83,"math":86,"physics":83,"chemistry":93,"IT":96},
+      {"id":4,"english": 77,"math":84,"physics":87,"chemistry":92,"IT":94},
+      {"id":5,"english": 76,"math":83,"physics":86,"chemistry":93,"IT":93},
+      {"id":6,"english": 86,"math":77,"physics":87,"chemistry":93,"IT":95},
+    ];
+  }*/
+  constructor() { }
+
+}
